45d2f1a195add7f34adc478211ae464f43e53808
Jason <Jason.Chen53@myhunter.cuny.edu>
2017-09-27 01:01:38 -0400

    updated encode string because i noticed how dum dum i am

--- 05/encode_string.py
+++ 05/encode_string.py
@@ -1,4 +1,4 @@
-def encode_letter(c, r):
+def encode_letter(c, r): # rotates lower case letters
     c = c.lower()
     num = ord(c)
     if (num + int(r) > 122): # checks if ord num passes ord of Z
@@ -9,7 +9,7 @@ def encode_letter(c, r):
         num = num + int(r) #else just do normally
     return chr(num)
 
-def encode_upper_letter(c, r):
+def encode_upper_letter(c, r): #rotates upper case letters
     num = ord(c)
     if (num + int(r) > 90): # checks if ord num passes ord of Z
         diff = 90 - int(num) #finds remaining amount till ord(z)
@@ -33,7 +33,26 @@ def encode_string(s, r):
                 string = string + new_letter
         new_string = new_string + string + ' '
         string = ''
-    return new_string # print string
+    return new_string # print string 
+
+''' --> you are a dum dum you could've just used a if (letter == ' ')
+    --> yeah i noticed after I finished the full encode.
+    =========================different and probably more easily followed code===========================
+    def encode_string(s, r):
+    new_string = ''
+    for letter in s:
+        if letter.isupper(): # check if letter is caps
+            new_letter = encode_upper_letter(letter, r) # if is cap then call encode_upper_letter
+            new_string = new_string + new_letter
+        elif letter == ' ': #checks for space
+            new_string = new_string + ' '
+        elif letter.isupper() == False: # if isn't cap call encode_letter
+            new_letter = encode_letter(letter, r)
+            new_string = new_string + new_letter
+
+    return new_string # print string  '''
+
 #string = input("enter a string: ")
 number = input("enter a number: ")
 print(encode_string('Hello Name is Bob', number))
+
--- 05/full_encode.py
+++ 05/full_encode.py
@@ -1,12 +1,15 @@
 def full_encodes(s):
     i = 0
     new_string = ''
-    while i < 26:
-        for letter in s:
-            new_string = new_string + encode_letter(letter, i)
-        print(new_string)
-        new_string = ''
-        i = i + 1
+    while i < 26: #26 characters in alphabet duh
+        for letter in s: #runs through each char of string
+            if (letter == ' '):
+                new_string = new_string + ' '
+            else:
+                new_string = new_string + encode_letter(letter, i) #encodes each char with the cooresponding i value
+        print(new_string) #prints rotated with respect to i
+        new_string = '' #reset new_string
+        i = i + 1 #i++
 
 def encode_letter(c, r):
     c = c.lower()
