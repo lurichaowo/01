6ed55363e515e6221c1212468f57db7d7d4c815d
Jason <Jason.Chen53@myhunter.cuny.edu>
2017-10-19 00:12:22 -0400

    finished ladybug

--- 08/ladybugs.py
+++ 08/ladybugs.py
@@ -2,37 +2,43 @@
 
 import sys
     
-def is_happy(board, cell):
-    if cell == 0:
-        if board[cell] == board[cell + 1]:
+def is_happy(b, cellnum):
+    if (cellnum != 0 and cellnum != len(b)-1):
+        if (b[cellnum+1] is b[cellnum] or b[cellnum-1] is b[cellnum]):
             return True
-        else:
-            return False
-    if (cell == len(board)):
-        if board[cell] == board[cell-1]:
-            return True
-        else:
-            return False
-    if (cell != 0 and cell != len(board)):
-        if (board[cell] == board[cell+1] or board[cell] == board[cell-1]):
-            return True
-        else:
-            return False
+    elif (cellnum == 0 and b[cellnum+1] is b[cellnum]):
+        return True
+    elif (cellnum == len(b) and b[cellnum-1] is b[cellnum]):
+        return True
+    else:
+        return False
     
-def search_for_empty(b, unhappy):
-    for space in b:
-        if space == '_':
-            space = unhappy
-            unhappy = '_'
-        
-def can_move(b):
-    for bug, cell in b:
-        if not is_happy(b, cell):
-            print("YES")
-            search_for_empty(b, bug)   
-            
+def poss(b, cellnum):
+    for i, num in enumerate(b):
+        if (i != cellnum and b[cellnum] == b[i]):
+            return True
+   
+def playable(b):
+    playable = True
+    if (b.count("_") == len(b)):
+        return True
+    for i, num in enumerate(b):
+        if num != "_":
+            if (b.count(num) == 1):
+                return False
+            elif not(is_happy(b, i)) and ("_" in b) and (poss(b, i)):
+                playable = True
+            elif not(is_happy(b,i)) and not(poss(b,i)):
+                playable = False
+            elif is_happy(b, i) and i == len(b):
+                playable = True 
+    return playable
+   
 Q = int(input().strip())
 for a0 in range(Q):
-    n = int(input().strip()) #board size 7
-    b = input().strip() #color of lady bugs/cells 'BYR_BYR'
-    can_move(b)
+    n = int(input().strip()) # number of cells
+    b = input().strip() # color of bugs
+    if playable(b):
+        print("YES")
+    elif not(playable(b)):
+        print("NO")
